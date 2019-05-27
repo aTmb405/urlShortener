ecac58bc883d5f8147cfeac1079061cea145a314
Glitch (automatic-constrictor) <none>
2019-05-27 20:51:03 +0000

    🎬📻 Checkpoint ./server.js:4579292/108

--- server.js
+++ server.js
@@ -27,8 +27,13 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/hello", function (req, res, next) {
+  res.json({
+    originalURL: 'hello API',
+    shortURL: 'hi API'
+  });
+}, function (req, res) {
+  
 });
 
 

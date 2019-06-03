6b3da6ce4a88edd9dbe90ebf7bd842c18009265e
Glitch (automatic-constrictor) <none>
2019-06-03 22:09:42 +0000

    🚥⏱ Checkpoint ./server.js:4579292/695 ./package.json:4579292/2313

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
-
+require('dotenv').config()
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
 
 var cors = require('cors');
 
@@ -12,14 +14,22 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+var urlSchema = new mongoose.Schema({
+  originalURL: String,
+    shortURL: String
+});
+
+var URL = mongoose.model('URL', urlSchema);
+
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
